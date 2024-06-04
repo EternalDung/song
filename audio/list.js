776558f70b34a1d5dcf462a66fca460599a030c8
Wei <191123654@qq.com>
2024-06-04 20:19:54 +0800

    add:Ang 5.0

---
+++ audio/list.js
@@ -0,0 +1,86 @@
+var list = [
+  {
+    name: "不想懂得",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/不想懂得.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "不想懂得（钢琴弦乐演奏版）",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/不想懂得（钢琴弦乐演奏版）.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "乐园",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/乐园.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "乐园(华尔兹弦乐演奏版)",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/乐园(华尔兹弦乐演奏版).mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "亲爱的，那不是爱情",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/亲爱的，那不是爱情.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "失忆",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/失忆.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "头号甜心",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/头号甜心.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "床边故事",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/床边故事.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "我恋爱了",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/我恋爱了.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "能不能勇敢说爱",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/能不能勇敢说爱.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "谁爱谁",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/谁爱谁.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+  {
+    name: "重来",
+    artist: "专辑-Ang 5.0",
+    url: "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/重来.mp3",
+    cover:
+      "https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/Ang 5.0/cover.png",
+  },
+];
---
+++ index.js
@@ -0,0 +1,24 @@
+var glob = require("glob")
+var fs = require("fs")
+ 
+glob("./audio/**/*.{mp3,wav,flac}", {}, function (er, files) {
+  let result = []
+  let download = ''
+  files.forEach(item => {
+    let fileInfo = fs.statSync(item)
+    if(fileInfo.size/(1024*1024) < 20) {    // 小于20M
+      let arr = item.split('/')
+      result.push({
+        name: arr[3].replace(/\.(mp3|flac|wav)$/g, ''),
+        artist: "专辑-"+arr[2],
+        url: 'https://testingcf.jsdelivr.net/gh/EternalDung/song@main' + item.slice(1),
+        cover: 'https://testingcf.jsdelivr.net/gh/EternalDung/song@main/audio/' + arr[2] + '/cover.png',
+      })
+      download += `https://testingcf.jsdelivr.net/gh/EternalDung/song@main${item.slice(1)}\n`
+    } else {
+      console.log('文件大于20M：', item)
+    }
+  })
+  fs.writeFileSync('./audio/list.js', "var list = " + JSON.stringify(result))
+  fs.writeFileSync('./audio/download.txt', download)
+})
